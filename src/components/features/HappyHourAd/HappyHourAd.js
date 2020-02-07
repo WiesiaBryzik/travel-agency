05447904735e65d804b9b52c1bb01b020d4aae03
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-02-08 00:21:29 +0100

    Add TDD 1 & 2

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '.HappyHourAd.scss';
+
+class HappyHourAd extends React.Component {
+  render() {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}></h3>
+        <div className={styles.promoDescription}></div>
+      </div>
+    );
+  }
+}
+
+export default HappyHourAd;

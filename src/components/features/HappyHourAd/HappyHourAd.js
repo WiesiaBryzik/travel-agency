9bc9c67d0d720198a29168700a6e65b9102da728
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-02-08 11:24:21 +0100

    Add TDD 5

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -3,19 +3,29 @@ import PropTypes from 'prop-types';
 import styles from '.HappyHourAd.scss';
 
 class HappyHourAd extends React.Component {
+
+  constructor() {
+    super();
+
+    /* run this.forceUpdate() every second */
+    setInterval(() => this.forceUpdate(), 1000);
+  }
+
+
+
   static propTypes = {
     title: PropTypes.node,
   };
 
-  getCountdownTime(){
+  getCountdownTime() {
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
 
-    if(currentTime.getUTCHours() >= 12){
-      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
     }
 
-    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
   }
 
   render() {
